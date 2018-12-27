956b13556f03033410357b9756f88b7ac9c1ce0c
Robin Krahl <robin.krahl@ireas.org>
2018-12-27 17:27:08 +0100

    Implement the pws clear subcommand
    This patch implements the pws clear command which allows the user to
    clear a slot in the password safe.

--- nitrocli/src/args.rs
+++ nitrocli/src/args.rs
@@ -326,6 +326,7 @@ impl str::FromStr for PinCommand {
 
 #[derive(Debug)]
 enum PwsCommand {
+  Clear,
   Get,
   Set,
 }
@@ -333,6 +334,7 @@ enum PwsCommand {
 impl PwsCommand {
   fn execute(&self, args: Vec<String>) -> Result<()> {
     match *self {
+      PwsCommand::Clear => pws_clear(args),
       PwsCommand::Get => pws_get(args),
       PwsCommand::Set => pws_set(args),
     }
@@ -345,6 +347,7 @@ impl fmt::Display for PwsCommand {
       f,
       "{}",
       match *self {
+        PwsCommand::Clear => "clear",
         PwsCommand::Get => "get",
         PwsCommand::Set => "set",
       }
@@ -357,6 +360,7 @@ impl str::FromStr for PwsCommand {
 
   fn from_str(s: &str) -> result::Result<Self, Self::Err> {
     match s {
+      "clear" => Ok(PwsCommand::Clear),
       "get" => Ok(PwsCommand::Get),
       "set" => Ok(PwsCommand::Set),
       _ => Err(()),
@@ -800,7 +804,7 @@ fn pws(args: Vec<String>) -> Result<()> {
   let _ = parser.refer(&mut subcommand).required().add_argument(
     "subcommand",
     argparse::Store,
-    "The subcommand to execute (get|set)",
+    "The subcommand to execute (clear|get|set)",
   );
   let _ = parser.refer(&mut subargs).add_argument(
     "arguments",
@@ -887,6 +891,22 @@ fn pws_set(args: Vec<String>) -> Result<()> {
   drop(parser);
 
   commands::pws_set(slot, &name, &login, &password)
+}
+
+/// Clear a PWS slot.
+fn pws_clear(args: Vec<String>) -> Result<()> {
+  let mut slot: u8 = 0;
+  let mut parser = argparse::ArgumentParser::new();
+  parser.set_description("Clears a password safe slot");
+  let _ = parser.refer(&mut slot).required().add_argument(
+    "slot",
+    argparse::Store,
+    "The PWS slot to clear",
+  );
+  parse(&parser, args)?;
+  drop(parser);
+
+  commands::pws_clear(slot)
 }
 
 /// Parse the command-line arguments and return the selected command and
