c24373901bd4feda581554a72e75a245b3af3361
Daniel Mueller <deso@posteo.net>
2019-01-08 18:15:11 -0800

    Add missing closing parenthesis for otp set -a/--algorithm help
    The help text for the otp set -a/--algorithm option is lacking the
    closing parenthesis in the help text. This change adds it.

--- nitrocli/src/args.rs
+++ nitrocli/src/args.rs
@@ -483,7 +483,7 @@ pub fn otp_set(ctx: &mut ExecCtx<'_>, args: Vec<String>) -> Result<()> {
   let _ = parser.refer(&mut algorithm).add_option(
     &["-a", "--algorithm"],
     argparse::Store,
-    "The OTP algorithm to use (hotp or totp, default: totp",
+    "The OTP algorithm to use (hotp or totp, default: totp)",
   );
   let _ = parser.refer(&mut name).required().add_argument(
     "name",
