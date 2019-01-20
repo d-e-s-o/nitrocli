d23b0b98ed896a7656b1f7fcdd34d4bbda5fad4f
Daniel Mueller <deso@posteo.net>
2019-01-20 14:52:00 -0800

    Add tests for the lock command
    This change adds tests for the lock command. For the Nitrokey Pro we
    cannot test too much because the only side-effect is that the password
    safe is closed and it will be opened automatically again by virtue of
    our non-interactive testing methodology.
    For Storage devices we verify that the encrypted volume is closed, which
    is a documented side-effect.

---
+++ nitrocli/src/tests/lock.rs
@@ -0,0 +1,43 @@
+// lock.rs
+
+// *************************************************************************
+// * Copyright (C) 2019 Daniel Mueller (deso@posteo.net)                   *
+// *                                                                       *
+// * This program is free software: you can redistribute it and/or modify  *
+// * it under the terms of the GNU General Public License as published by  *
+// * the Free Software Foundation, either version 3 of the License, or     *
+// * (at your option) any later version.                                   *
+// *                                                                       *
+// * This program is distributed in the hope that it will be useful,       *
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+// * GNU General Public License for more details.                          *
+// *                                                                       *
+// * You should have received a copy of the GNU General Public License     *
+// * along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+// *************************************************************************
+
+use super::*;
+
+#[test_device]
+fn lock_pro(device: nitrokey::Pro) -> crate::Result<()> {
+  // We can't really test much more here than just success of the command.
+  let out = Nitrocli::with_dev(device).handle(&["lock"])?;
+  assert!(out.is_empty());
+
+  Ok(())
+}
+
+#[test_device]
+fn lock_storage(device: nitrokey::Storage) -> crate::Result<()> {
+  let mut ncli = Nitrocli::with_dev(device);
+  let _ = ncli.handle(&["storage", "open"])?;
+
+  let out = ncli.handle(&["lock"])?;
+  assert!(out.is_empty());
+
+  let device = nitrokey::Storage::connect()?;
+  assert!(!device.get_status()?.encrypted_volume.active);
+
+  Ok(())
+}
