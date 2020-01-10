993bef29bf9cc7c7d9a4bd5b97d5ea9ba2d44b04
Daniel Mueller <deso@posteo.net>
2020-01-10 10:39:03 -0800

    fixup! Replace argparse with structopt

--- nitrocli/src/tests/config.rs
+++ nitrocli/src/tests/config.rs
@@ -1,7 +1,7 @@
 // config.rs
 
 // *************************************************************************
-// * Copyright (C) 2019 Daniel Mueller (deso@posteo.net)                   *
+// * Copyright (C) 2019-2020 Daniel Mueller (deso@posteo.net)              *
 // *                                                                       *
 // * This program is free software: you can redistribute it and/or modify  *
 // * it under the terms of the GNU General Public License as published by  *
