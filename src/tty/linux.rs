62a4e4522b86c81c51ee004474e7d4e7d6fada4e
Daniel Mueller <deso@posteo.net>
2022-06-16 21:30:26 -0700

    Factor out TTY logic into separate module
    The logic we have in place for retrieving the path to the TTY to set
    the GPG_TTY environment variable to if it is currently unset is rather
    complex and very specific to the operating system used. In addition, for
    better or worse we have plans to extend further, necessitating the usage
    of even more OS specific functionality.
    In order to not pollute the pinentry module with too much of this logic,
    this change moves this functionality into a new module, tty.
    Furthermore, in an attempt to maintain a clear separation between
    the support code for different operating systems, we split this module
    into different sub-modules that are compiled conditionally.

---
+++ src/tty/linux.rs
@@ -0,0 +1,75 @@
+// linux.rs
+
+// Copyright (C) 2022 The Nitrocli Developers
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use std::ffi;
+use std::io;
+use std::os::unix::ffi::OsStringExt as _;
+use std::os::unix::io::AsRawFd as _;
+
+/// Retrieve a path to the TTY used for stdin, if any.
+///
+/// This function works on a best effort basis and skips any advanced
+/// error reporting, knowing that callers do not care.
+pub(crate) fn retrieve_tty() -> Result<ffi::OsString, ()> {
+  let fd = io::stdin().as_raw_fd();
+  let fd_path = format!("/proc/self/fd/{}\0", fd);
+  let fd_path = ffi::CStr::from_bytes_with_nul(fd_path.as_bytes()).unwrap();
+
+  let mut buffer = Vec::<u8>::with_capacity(56);
+  // SAFETY: We provide valid pointers, `fd_path` is NUL terminated, and
+  //         the provided capacity reflects the actual length of the
+  //         buffer.
+  let rc = unsafe {
+    libc::readlink(
+      fd_path.as_ptr(),
+      buffer.as_mut_ptr() as *mut libc::c_char,
+      buffer.capacity(),
+    )
+  };
+  if rc <= 0 {
+    return Err(());
+  }
+
+  let rc = rc as usize;
+  // If `readlink` filled the entire buffer we could have experienced
+  // silent truncation. So we just bail out out of precaution.
+  if rc == buffer.capacity() {
+    return Err(());
+  }
+
+  // SAFETY: At this point we know that `readlink` has written `rc`
+  //         bytes to `buffer`.
+  unsafe { buffer.set_len(rc) };
+
+  Ok(ffi::OsString::from_vec(buffer))
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  use std::fs;
+
+  /// Check that we can retrieve the path to the TTY used for stdin.
+  #[test]
+  fn tty_retrieval() {
+    // We may be run with stdin not referring to a TTY in CI.
+    if unsafe { libc::isatty(io::stdin().as_raw_fd()) } == 0 {
+      return;
+    }
+
+    let tty = retrieve_tty().unwrap();
+    // To check sanity of the reported path at least somewhat, we just
+    // try opening the file, which should be possible. Note that we open
+    // in write mode, because for one reason or another we would not
+    // actually fail opening a *directory* in read-only mode.
+    let _file = fs::OpenOptions::new()
+      .create(false)
+      .write(true)
+      .read(true)
+      .open(tty)
+      .unwrap();
+  }
+}
