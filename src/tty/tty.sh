2688f52774bc8de4603c304217d9b0a691ee2a8f
Daniel Mueller <deso@posteo.net>
2022-06-16 22:21:35 -0700

    Walk process tree as part of TTY inference
    Unless the GPG_TTY environment variable is set by the user, we try our
    best to find the process' TTY ourselves and set the environment variable
    accordingly. However, so far we only looked at our own process' stdin to
    see whether it refers to a TTY.
    Alas, we could have been invoked by another process and stdin could have
    been redirected arbitrarily in such a case. git-send-email, for example,
    does such a dance. If that's the case, we'd end up reporting a file as a
    TTY candidate that isn't really a TTY.
    It seems somewhat sensible to just "do the right thing" and walk up the
    process hierarchy until we find "the" TTY to use (at least until we get
    a report where that breaks). With this change we do exactly that.

---
+++ src/tty/tty.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Copyright (C) 2022 The Nitrocli Developers
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# We support testing both /dev/null and pipe redirection of stdin and
+# the first argument controls which one to use.
+stdin=${1:-"pipe"}
+instance=${2:-1}
+
+if [ $instance -ge 5 ]; then
+  # Print own process ID and then just wait for a while until we get
+  # killed.
+  echo $$
+  sleep 60
+else
+  # Invoke the script recursively, doing an actual fork and not just an
+  # exec in order to spawn a new process. Also redirect stdin to
+  # simulate it not referring to a TTY directly.
+  if [ $stdin != "pipe" ]; then
+    sh $0 $stdin $((instance + 1)) < /dev/null
+  else
+    cat | sh $0 $stdin $((instance + 1))
+  fi
+fi
