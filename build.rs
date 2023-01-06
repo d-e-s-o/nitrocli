7991b9cb4a8fee2ea5ffd11f6fed2bd7f5cded21
Daniel Müller <deso@posteo.net>
2023-01-05 20:15:24 -0800

    Switch to using grev::git_revision_auto() function in build.rs
    With the release of version 0.1.3 of the grev crate, the git_revision()
    function that we were relying on in our build script has been
    deprecated.
    With this change we replace its usage with the recommended
    git_revision_auto() function instead.

--- build.rs
+++ build.rs
@@ -1,23 +1,14 @@
-// Copyright (C) 2021-2022 The Nitrocli Developers
+// Copyright (C) 2021-2023 The Nitrocli Developers
 // SPDX-License-Identifier: GPL-3.0-or-later
-
-use std::io::stdout;
 
 use anyhow::Result;
 
-use grev::get_revision as get_git_revision;
+use grev::git_revision_auto;
 
 fn main() -> Result<()> {
   let directory = env!("CARGO_MANIFEST_DIR");
-  if let Some(git_revision) = get_git_revision(directory, stdout().lock())? {
+  if let Some(git_revision) = git_revision_auto(directory)? {
     println!("cargo:rustc-env=NITROCLI_GIT_REVISION={}", git_revision);
   }
-  // Make sure to run this script again if any of our sources files
-  // changes.
-  println!("cargo:rerun-if-changed=Cargo.lock");
-  println!("cargo:rerun-if-changed=Cargo.toml");
-  println!("cargo:rerun-if-changed=ext/");
-  println!("cargo:rerun-if-changed=src/");
-  println!("cargo:rerun-if-changed=var/");
   Ok(())
 }
