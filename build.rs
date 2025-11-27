d6027c4fea5f4d27d0917dd05c35b630b5d602e7
Daniel Müller <deso@posteo.net>
2025-11-27 06:43:04 -0800

    cli: Use runtime environment variable query in build script
    It is incorrect to assume presence of the various environment variables
    used in the build script snippet at compile time [0]. Switch to using
    runtime querying instead.
    
    [0] https://doc.rust-lang.org/cargo/reference/environment-variables.html#environment-variables-cargo-sets-for-build-scripts

--- build.rs
+++ build.rs
@@ -1,13 +1,17 @@
-// Copyright (C) 2021-2023 The Nitrocli Developers
+// Copyright (C) 2021-2025 The Nitrocli Developers
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+use std::env;
+
+use anyhow::Context as _;
 use anyhow::Result;
 
 use grev::git_revision_auto;
 
 fn main() -> Result<()> {
-  let directory = env!("CARGO_MANIFEST_DIR");
-  if let Some(git_revision) = git_revision_auto(directory)? {
+  let manifest_dir =
+    env::var_os("CARGO_MANIFEST_DIR").context("CARGO_MANIFEST_DIR variable not set")?;
+  if let Some(git_revision) = git_revision_auto(manifest_dir)? {
     println!("cargo:rustc-env=NITROCLI_GIT_REVISION={}", git_revision);
   }
   Ok(())
