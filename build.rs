00d4b330b6e060ba50b1e3859fe2c060a2c71627
Daniel Müller <deso@posteo.net>
2026-09-11 23:40:36 -0700

    Update grev crate to 0.2
    Update the grev crate to version 0.2 to get the latest and greatest.

--- build.rs
+++ build.rs
@@ -1,18 +1,16 @@
-// Copyright (C) 2021-2025 The Nitrocli Developers
+// Copyright (C) 2021-2026 The Nitrocli Developers
 // SPDX-License-Identifier: GPL-3.0-or-later
 
 use std::env;
 
-use anyhow::Context as _;
-use anyhow::Result;
+use grev::git_revision;
 
-use grev::git_revision_auto;
-
-fn main() -> Result<()> {
+fn main() {
   let manifest_dir =
-    env::var_os("CARGO_MANIFEST_DIR").context("CARGO_MANIFEST_DIR variable not set")?;
-  if let Some(git_revision) = git_revision_auto(manifest_dir)? {
-    println!("cargo:rustc-env=NITROCLI_GIT_REVISION={}", git_revision);
+    env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR variable not set");
+
+  let git_rev = git_revision(manifest_dir).expect("failed to query Git revision");
+  if let Some(git_rev) = git_rev {
+    println!("cargo:rustc-env=NITROCLI_GIT_REVISION={}", git_rev);
   }
-  Ok(())
 }
